cd45082f43b63361fdbc19f678c422f9d25fe275
Dennis Riehle <webmaster@riehle-web.com>
2025-01-20 23:26:39 +0100

    set branch in CI for composer to detect root version
    Signed-off-by: Dennis Riehle <webmaster@riehle-web.com>

--- .laminas-ci/pre-install.sh
+++ .laminas-ci/pre-install.sh
@@ -2,17 +2,10 @@
 
 WORKING_DIRECTORY=$2
 JOB=$3
-PHP_VERSION=$(php -nr "echo PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION;")
+PHP_VERSION=$(echo "${JOB}" | jq -r '.php')
 
-if [ ! -z "$GITHUB_BASE_REF" ] && [[ "$GITHUB_BASE_REF" =~ ^[0-9]+\.[0-9] ]]; then
-  readarray -td. TARGET_BRANCH_VERSION_PARTS <<<"${GITHUB_BASE_REF}.";
-  unset 'TARGET_BRANCH_VERSION_PARTS[-1]';
-  declare -a TARGET_BRANCH_VERSION_PARTS
-  MAJOR_OF_TARGET_BRANCH=${TARGET_BRANCH_VERSION_PARTS[0]}
-  MINOR_OF_TARGET_BRANCH=${TARGET_BRANCH_VERSION_PARTS[1]}
-
-  export COMPOSER_ROOT_VERISON="${MAJOR_OF_TARGET_BRANCH}.${MINOR_OF_TARGET_BRANCH}.99"
-  echo "Exported COMPOSER_ROOT_VERISON as ${COMPOSER_ROOT_VERISON}"
-fi
+# Temporary workaround for cyclic dependencies - must be adjusted in next major branch!
+echo "Branch as 3.99.x in Pre-Install"
+git checkout -b 3.99.x
 
 ${WORKING_DIRECTORY}/.laminas-ci/install-apcu-extension-via-pecl.sh "${PHP_VERSION}" || exit 1
